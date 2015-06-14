1df5dd33916f92260c136721e59e60cdbb28c8b8
Mark Bouslog <mcbouslog@gmail.com>
2015-06-14 13:35:00 -0500

    Create Category and CategorizedProduct models

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150610023537) do
+ActiveRecord::Schema.define(version: 20150614183209) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "categorized_products", force: :cascade do |t|
+    t.integer  "product_id",  limit: 4
+    t.integer  "category_id", limit: 4
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "orders", force: :cascade do |t|
     t.integer  "quantity",    limit: 4
