07f06d113c7d71c239a24274769c88e311c74c14
Mark Bouslog <mcbouslog@gmail.com>
2015-06-14 17:25:15 -0500

    seeds.db update

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,22 @@
+CategorizedProduct.create!([
+  {product_id: 1, category_id: 1},
+  {product_id: 2, category_id: 1},
+  {product_id: 3, category_id: 1},
+  {product_id: 4, category_id: 2},
+  {product_id: 5, category_id: 3},
+  {product_id: 6, category_id: 2},
+  {product_id: 7, category_id: 3},
+  {product_id: 8, category_id: 3},
+  {product_id: 9, category_id: 1},
+  {product_id: 4, category_id: 3},
+  {product_id: 8, category_id: 2},
+  {product_id: 9, category_id: 2}
+])
+Category.create!([
+  {name: "Baby Supplies"},
+  {name: "Alcohol"},
+  {name: "Food"}
+])
 Order.create!([
   {quantity: 2, product_id: 6, user_id: 1, total_price: "19.6", tax: "1.62", subtotal: "17.98"},
   {quantity: 10, product_id: 9, user_id: 1, total_price: "92.5", tax: "7.6", subtotal: "84.9"},
