1962f2b547ceb6ab41a754702b12a81b406f9abb
Mark Bouslog <mcbouslog@gmail.com>
2015-06-14 10:37:27 -0500

    Update migrations

--- db/migrate/20150614152623_change_price_to_decimal_in_products.rb
+++
@@ -1,5 +0,0 @@
-class ChangePriceToDecimalInProducts < ActiveRecord::Migration
-  def change
-    change_column :products, :price, 'decimal USING CAST(column_name AS decimal)', precision: 8, scale: 2
-  end
-end
